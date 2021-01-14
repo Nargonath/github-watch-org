920c2e95aa6d313877c471cec000baa0cbd299ed
Nargonath <jonas.pauthier@gmail.com>
2021-01-14 15:17:58 +0100

    wip: login command

--- utils/cliHandler.js
+++ utils/cliHandler.js
@@ -7,6 +7,9 @@ module.exports = meow(
     Usage
       $ gwo [options] <command>
 
+    Commands
+      login  Asks for your credentials and logs you in your GH account
+
     Options
       -h, --help Displays help
   `,
